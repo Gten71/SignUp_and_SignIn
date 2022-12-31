3e1cd1a2919f5fd9d23817ae1172a3bfcaf052fe
Gten71 <mresnyansky@gmail.com>
2022-12-31 14:55:16 +0300

    add progress bar and change first screen

--- app/src/main/java/com/example/log/LoginActivity.kt
+++ app/src/main/java/com/example/log/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.log
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -15,14 +16,21 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var progressDialog:ProgressDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         // Initialize Firebase Auth
         auth = Firebase.auth
 
+        progressDialog = ProgressDialog(this,)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Loggin in...")
+        progressDialog.setCanceledOnTouchOutside(false)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
         val registertext: TextView =findViewById(R.id.register_now)
         registertext.setOnClickListener{
             val intent = Intent(this,RegiterActivity::class.java)
@@ -35,6 +43,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun performSignIn() {
+        progressDialog.show()
         val email: EditText = findViewById<EditText>(R.id.username)
         val password:EditText = findViewById<EditText>(R.id.password)
 
@@ -46,11 +55,20 @@ class LoginActivity : AppCompatActivity() {
         val inputEmail = email.text.toString()
         val inputPassword = password.text.toString()
         auth.signInWithEmailAndPassword(inputEmail, inputPassword)
+            .addOnSuccessListener {
+                progressDialog.dismiss()
+                val firebaseUser = auth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this,"LogIn as $email",Toast.LENGTH_LONG).show()
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+
+            }
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     // Sign in success, update UI with the signed-in user's information
-                    val intent=Intent(this,MainActivity::class.java)
-                    startActivity(intent)
+//                    val intent=Intent(this,MainActivity::class.java)
+//                    startActivity(intent)
 
                     Toast.makeText(baseContext, "Authentication success.",
                         Toast.LENGTH_SHORT).show()
@@ -65,7 +83,14 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
             .addOnFailureListener{
+                progressDialog.dismiss()
                 Toast.makeText(this,"Error occurred${it.localizedMessage}",Toast.LENGTH_SHORT).show()
             }
+    }
+    private fun checkUser(){
+        val firebaseUser = auth.currentUser
+        if (firebaseUser !=null)
+            startActivity(Intent(this,MainActivity::class.java))
+        finish()
     }
 }
